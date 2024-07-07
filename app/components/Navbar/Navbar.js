b455374cfb68ed592da8d1c3f25eecb99cc323f5
vikashchander <vikashchander78384@gmail.com>
2024-07-07 14:54:53 +0530

    show category and prodts in the nav bar

--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,16 +1,26 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import {
   MDBContainer,
   MDBNavbar,
-  MDBNavbarBrand
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+  MDBNavbarToggler,
+  MDBIcon
 } from 'mdb-react-ui-kit';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const [openNav, setOpenNav] = useState(false);
+  const pathname = usePathname();
+  console.log(pathname, "dsfs???")
   return (
     <>
-      <MDBNavbar sticky light bgColor='light'>
+      <MDBNavbar  expand='lg' sticky light bgColor='light'>
         <MDBContainer fluid>
           <MDBNavbarBrand href='/'>
             <Image
@@ -22,6 +32,28 @@ export default function Navbar() {
             />
             <h6>Khatri Jewellers</h6>
           </MDBNavbarBrand>
+              <MDBNavbarToggler
+                  type='button'
+                  aria-expanded='false'
+                  aria-label='Toggle navigation'
+                  onClick={() => setOpenNav(!openNav)}
+              >
+                <MDBIcon icon='bars' fas />
+              </MDBNavbarToggler>
+            <MDBCollapse navbar open={openNav}>
+              <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href='/category/lists' active={pathname === '/category/lists'} aria-current='page'>
+                  Category
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink  href='/product/lists' active={pathname === '/product/lists'} aria-current='page'>
+                    Products
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              </MDBNavbarNav>
+            </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
     </>
